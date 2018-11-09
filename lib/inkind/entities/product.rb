41d51de8b879bc7beaa1e5b8123bd8b77e6d9999
Nicolas Dreux <nicolas.dreux@cg.nl>
2018-11-09 10:06:24 +0100

    Add a type in a product. Can be voucher, recharge, payment Remove unnecessary log

--- lib/inkind/entities/product.rb
+++ lib/inkind/entities/product.rb
@@ -5,7 +5,7 @@ module InkindApi
     class Product
       attr_accessor :id, :name, :short_description,
                     :country, :operator, :service,
-                    :currency, :account_currency
+                    :currency, :account_currency, :type
 
       def initialize(product)
         parameters = product_parameters product
@@ -15,6 +15,7 @@ module InkindApi
         @short_description = parameters['product_short_desc']
         @currency          = parameters['product_currency']
         @account_currency  = parameters['account_currency']
+        @type              = parameters['type']
 
         @operator = Operator.new(product)
         @country  = Country.new(product)
@@ -26,10 +27,8 @@ module InkindApi
       def product_parameters(product)
         product.slice('product_id', 'product_name', 'product_short_desc',
                       'operator_id', 'operator', 'country_id', 'country',
-                      'service_id', 'service', 'product_value',
-                      'product_currency', 'local_value', 'local_currency',
-                      'account_currency', 'wholesale_price', 'retail_price',
-                      'fee')
+                      'service_id', 'service',
+                      'product_currency', 'account_currency', 'type')
       end
     end
   end
--- lib/inkind/factories/product_factory.rb
+++ lib/inkind/factories/product_factory.rb
@@ -4,15 +4,25 @@ module InkindApi
   module Factory
     class Product
       def self.create(type, product_data, suggested_values = [])
+        product_data['type'] = get_product_type(type)
+
+        product = nil
+
         case type
         when 'fixed_value_vouchers', 'fixed_value_recharges'
           product = Entity::FixedValueProduct.new product_data
         when 'variable_value_payments', 'variable_value_recharges'
           product = Entity::VariableValueProduct.new(product_data, suggested_values)
-        else
-          raise StandardError.new, "This product type is not managed: #{type}"
         end
         product
+      end
+
+      def self.get_product_type(type)
+        unless %w[fixed_value_recharges fixed_value_vouchers variable_value_recharges variable_value_payments].include?(type)
+          raise StandardError.new, "This product type is not managed: #{type}"
+        end
+
+        type.split('_')[2].singularize
       end
     end
   end

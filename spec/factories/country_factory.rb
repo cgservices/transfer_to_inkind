3f091d20679de7be749603d5caf6b6e4983e2513
Nicolas Dreux <nicolas.dreux@cg.nl>
2018-11-14 08:04:56 +0100

    Specify classes for factories Improve initialize method for the suggested_value class

--- spec/factories/country_factory.rb
+++ spec/factories/country_factory.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :country do
+  factory :country, class: 'InkindApi::Entity::Country' do
     id { Faker::Number(5) }
     name { Faker::Address.country }
   end
--- spec/factories/service_factory.rb
+++ spec/factories/service_factory.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :service do
+  factory :service, class: 'InkindApi::Entity::Service' do
     id { Faker::Number(5) }
     name { Faker::Lorem.word }
   end

24157a3e67c857b87c229675f8bdef3c48e5515a
Nicolas Dreux <nicolas.dreux@cg.nl>
2018-11-14 08:05:29 +0100

    Add the min/max method for the suggested values

--- lib/inkind/entities/variable_value_product.rb
+++ lib/inkind/entities/variable_value_product.rb
@@ -9,6 +9,22 @@ module InkindApi
         super(product)
         @suggested_values = suggested_values
       end
+
+      def min(criterion)
+        min_suggested_value_by(criterion).public_send(criterion)
+      end
+
+      def max(criterion)
+        max_suggested_value_by(criterion).public_send(criterion)
+      end
+
+      def min_suggested_value_by(criterion)
+        @suggested_values.min_by(&criterion)
+      end
+
+      def max_suggested_value_by(criterion)
+        @suggested_values.max_by(&criterion)
+      end
     end
   end
 end

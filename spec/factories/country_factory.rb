1d4dd9a0511648b16afdd8062c453a8a24b51713
Nicolas Dreux <nicolas.dreux@cg.nl>
2018-11-16 08:07:44 +0100

    Fix factories

--- spec/factories/country_factory.rb
+++ spec/factories/country_factory.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :country, class: 'InkindApi::Entity::Country' do
+  factory :country, class: InkindApi::Entity::Country do
     id { Faker::Number(5) }
     name { Faker::Address.country }
+    initialize_with { new({}) }
   end
 end
--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -2,20 +2,22 @@
 
 FactoryBot.define do
   currency = Faker::Currency.code
-  factory :product, class: 'InkindApi::Entity::Product' do
-    id { Faker::Number(5) }
+  factory :product, class: InkindApi::Entity::Product do
+    id { Faker::Number.number(5) }
     name { Faker::Lorem.word }
     short_description { Faker::Lorem.sentence }
     currency { currency }
     account_currency { currency }
     type { %w[voucher recharge payment].sample }
 
-    operator
-    country
-    service
+    operator { build :operator }
+    country { build :country }
+    service { build :service }
+
+    initialize_with { new({}) }
   end
 
-  factory :fixed_value_product, parent: :product, class: 'InkindApi::Entity::FixedValueProduct' do
+  factory :fixed_value_product, parent: :product, class: InkindApi::Entity::FixedValueProduct do
     value { Faker::Number.decimal(2) }
     local_value { Faker::Number.decimal(2) }
     local_currency { currency }
@@ -24,7 +26,7 @@ FactoryBot.define do
     fee { Faker::Number.decimal(2) }
   end
 
-  factory :variable_value_product, parent: :product, class: 'InkindApi::Entity::VariableValueProduct' do
-    suggested_values { create_list(:suggested_value, 2) }
+  factory :variable_value_product, parent: :product, class: InkindApi::Entity::VariableValueProduct do
+    suggested_values { build_list(:suggested_value, 2) }
   end
 end

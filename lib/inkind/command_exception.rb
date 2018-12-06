7e90c34f1b97573ddaf8fafffc87bb2997f42353
Nicolas Dreux <nicolas.dreux@cg.nl>
2018-12-06 12:35:52 +0100

    Remove raise ApiException

--- lib/inkind/command_exception.rb
+++ lib/inkind/command_exception.rb
@@ -47,8 +47,6 @@ module InkindApi
       attr_accessor :code, :message, :raw_response
 
       def initialize(code, message, raw_response)
-        raise ApiResponseError(raw_response) unless code.present? && message.present?
-
         @code         = code
         @message      = message
         @raw_response = raw_response

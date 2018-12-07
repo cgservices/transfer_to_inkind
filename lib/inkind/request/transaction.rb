f58c03cd347afb6bec03f4ceb1cee3b416c52035
Nicolas Dreux <nicolas.dreux@cg.nl>
2018-12-07 09:30:01 +0100

    Improve: Error message with an invalid FixedValueVoucher

--- lib/inkind/request/transaction.rb
+++ lib/inkind/request/transaction.rb
@@ -4,7 +4,7 @@ module InkindApi
       def fixed_value_voucher(attributes:)
         request = InkindApi::Entity::Request::FixedValueVoucher.new(attributes)
 
-        raise StandardError, "Invalid FixedValueVoucher request parameters. Data #{request.meta_data}" unless request.valid?
+        raise StandardError, "Invalid FixedValueVoucher request parameters: #{request.meta_data}" unless request.valid?
 
         post('transactions/fixed_value_vouchers', request.body) do |json|
           return InkindApi::Entity::Response::FixedValueVoucher.new(json)

6378f8cacabbbe908a1f44d77fe45836ed3ecb17
Nicolas Dreux <nicolas.dreux@cg.nl>
2018-12-13 12:08:13 +0100

    RAP-1450 - Improve response result when requesting variable_value_recharges

--- lib/inkind/entity/response/variable_value_recharge.rb
+++ lib/inkind/entity/response/variable_value_recharge.rb
@@ -8,12 +8,16 @@ module Inkind
           super(parameters)
           attributes = filter_parameters(parameters)
 
-          @product     = Inkind::Factory::Entity::Product.create('variable_value_recharges', parameters)
-          @local_value = attributes['local_value']
+          @product         = Inkind::Factory::Entity::Product.create('variable_value_recharges', parameters)
+          @local_value     = attributes['local_value']
+          @local_currency  = attributes['local_currency']
+          @wholesale_price = attributes['wholesale_price']
+          @retail_price    = attributes['retail_price']
+          @fee             = attributes['fee']
         end
 
         def filter_parameters(parameters)
-          fields = %w(local_value)
+          fields = %w[local_value local_currency wholesale_price retail_price fee]
           parameters.select { |k, _v| fields.include?(k) }
         end
       end

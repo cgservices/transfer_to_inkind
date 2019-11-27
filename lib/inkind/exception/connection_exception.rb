b1c65d7cfb04ccbd9357df157648b35d4a6ce921
Nicolas Dreux <nclsdrx@gmail.com>
2019-11-27 11:31:32 +0100

    Fine-tune Connection Exceptions
    A new TimeoutException has been defined in order to separated connection exception (reached open timeout,  SSL error...) from time out exception (read timeout).

--- lib/inkind/exception/connection_exception.rb
+++ lib/inkind/exception/connection_exception.rb
@@ -1,5 +1,14 @@
 module Inkind
   module Exception
-    class ConnectionException < StandardError; end
+    class ConnectionException < RuntimeError
+
+      attr_reader :raw_response
+
+      def initialize(raw_response = nil)
+        @raw_response = raw_response
+      end
+    end
+
+    class TimeoutException < ConnectionException; end
   end
 end
